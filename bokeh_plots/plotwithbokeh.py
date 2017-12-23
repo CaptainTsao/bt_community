958803de1236a623fb66c5813c4975a5e14eb886
Andrey Blinkov <andreyblinkov@gmail.com>
2017-12-22 23:44:30 -0800

    plotwithbokeh.py, version 0.5.156 - changed script description - added parameters for figure's height and width - consider data.plotinfo.plot parameter - added candlestick diagram plotting (candles_plot()) - added volume diagram plotting (volume_plot())

--- bokeh_plots/plotwithbokeh.py
+++ bokeh_plots/plotwithbokeh.py
@@ -1,8 +1,8 @@
 '''
-Backtrader analyzer for plotting using Bokeh interactive plotting library
+Backtrader plotter using Bokeh interactive plotting library
 
 Created: 2017-12-16
-Updated: 2017-12-21
+Updated: 2017-12-22
 
 Created by ab-trader
 '''
@@ -12,14 +12,23 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 from bokeh.models.widgets import Panel, Tabs
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
+from bokeh.layouts import gridplot
 
-__version__ = '0.4.105'
+from math import pi
+
+import backtrader as bt
+import pandas as pd
+import pdb
+
+__version__ = '0.5.156'
 
 class btBokehPlot():
 
     def __init__(self, **kwargs):
 
-        print('Bokeh plotter version %s' % __version__)
+        self.width = 800
+        self.q_height = 300
+        self.v_height = 150
 
 
     def plot(self, strategy, figid=0, numfigs=1, iplot=True, start=None, end=None, use=None):
@@ -40,14 +49,20 @@ class btBokehPlot():
         if not len(strategy):
             return
 
-        output_file('strategy_%d.html' % figid)
+        self.dt_axis = [bt.num2date(x) for x in strategy.datetime.array.tolist()]
+
+        output_file('strategy_%d.html' % figid, title='strategy #%d' % figid)
         
         strategy_tabs = []
         strategy_tabs.append(self.broker_tab(name='BROKER'))
 
         for data in strategy.datas:
-            strategy_tabs.append(self.data_tab(data=data))
+            if data.plotinfo.plot:
+                strategy_tabs.append(self.data_tab(data=data))
         
+        if not len(strategy_tabs):
+            return
+
         tabs = Tabs(tabs=strategy_tabs)
         
         show(tabs)
@@ -80,26 +95,62 @@ class btBokehPlot():
         Creates tab with data feed (prices) diagram shown in candle format.
         "name" is the name of the tab
         '''
-        tab = Panel(child=self.quotes_plot(data), title=data._name)
+        # prepare data
+        df = pd.DataFrame(index=xrange(len(data)))
+        df['open'] = data.open.array.tolist()
+        df['high'] = data.high.array.tolist()
+        df['low'] = data.low.array.tolist()
+        df['close'] = data.close.array.tolist()
+        df['volume'] = data.volume.array.tolist()
+        dt_list = [bt.num2date(x) for x in data.datetime.array.tolist()]
+        df['dt'] = dt_list
+
+        quotes = self.candles_plot(df)
+        volume = self.volume_plot(df, quotes)
+
+        grid = gridplot([quotes, volume], ncols=1)
+        tab = Panel(child=grid, title=data._name)
 
         return tab
 
 
-    def quotes_plot(self, data):
+    def candles_plot(self, df):
         '''
-        Creates quotes figure based on backtrader's datafeed "data"
+        Creates candlestick figure for backtrader's datafeed "data"
         '''
-        quotes_fig = figure(plot_width=300, plot_height=300)
-        quotes_fig.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
+        inc = df.close > df.open
+        dec = df.open > df.close
+        w = 12*60*60*1000 # half day in ms
 
-        return quotes_fig
+        fig = figure(x_axis_type="datetime", plot_width=self.width, plot_height=self.q_height,
+                     x_range=[self.dt_axis[0], self.dt_axis[-1]])
+        fig.xaxis.major_label_orientation = pi/4
+        fig.grid.grid_line_alpha=1.0
+
+        fig.segment(df.dt, df.high, df.dt, df.low, color="black")
+        fig.vbar(df.dt[inc], w, df.open[inc], df.close[inc],
+                 fill_color="white", line_color="black")
+        fig.vbar(df.dt[dec], w, df.open[dec], df.close[dec],
+                 fill_color="#2d2d2d", line_color="black")
+
+        return fig
 
 
-    def volume_plot(self, data, connected_data_figure):
+    def volume_plot(self, df, connected_data_figure):
         '''
-        Creates volume figure connected to "data" figure based on backtrader's datafeed "data"
+        Creates volume figure connected to "data" figure for backtrader's datafeed "data"
         '''
-        volume_fig = figure(plot_width=300, plot_height=300)
-        volume_fig.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
+        inc = df.close > df.open
+        dec = df.open > df.close
+        w = 18*60*60*1000 # half day in ms
 
-        return volume_fig
+        fig = figure(x_axis_type="datetime", plot_width=self.width, plot_height=self.v_height,
+                     x_range=connected_data_figure.x_range)
+        fig.xaxis.major_label_orientation = pi/4
+        fig.grid.grid_line_alpha=1.0
+        fig.vbar(df.dt[inc], w, 0, df.volume[inc], fill_color="white", line_color="black",
+                 line_width=0.5)
+        fig.vbar(df.dt[dec], w, 0, df.volume[dec], fill_color="#2d2d2d", line_color="black",
+                 line_width=0.5)
+
+        return fig
